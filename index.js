562c3ef72002de1df99b0aeb4fa57a2040728599
Nicholas Speeter <nicholas.speeter@commercetools.de>
2020-09-14 13:01:34 -0400

    feature: create extension/subscription after deploy to aws

--- index.js
+++ index.js
@@ -1,65 +1,70 @@
-"use strict";
+const requestBuilder = require("@commercetools/api-request-builder");
+const clientCreate = require("@commercetools/sdk-client");
+const middlewareAuth = require("@commercetools/sdk-middleware-auth");
+const createMiddleware = require("@commercetools/sdk-middleware-http");
+const clientQueue = require("@commercetools/sdk-middleware-queue");
+("use strict");
 
 class ServerlessPlugin {
   constructor(serverless, options) {
     this.serverless = serverless;
     this.options = options;
-
-    this.commands = {
-      welcome: {
-        usage: "Helps you start your first Serverless plugin",
-        lifecycleEvents: ["hello", "world"],
-        options: {
-          message: {
-            usage:
-              "Specify the message you want to deploy " +
-              "(e.g. \"--message 'My Message'\" or \"-m 'My Message'\")",
-            required: true,
-            shortcut: "m",
-          },
-        },
-      },
-    };
-
+    this.projectKey = this.serverless.service.provider.environment.CTP_PROJECT_KEY;
+    this.clientId = this.serverless.service.provider.environment.CTP_CLIENT_ID;
+    this.clientSecret = this.serverless.service.provider.environment.CTP_CLIENT_SECRET;
+    this.apiUrl = this.serverless.service.provider.environment.CTP_API_URL;
+    this.authURL = this.serverless.service.provider.environment.CTP_AUTH_URL;
+    this.scopes = this.serverless.service.provider.environment.CTP_SCOPES;
+    this.deployType = this.serverless.service.provider.environment.CTP_DEPLOY_TYPE;
+    this.postBody = this.serverless.service.provider.environment.CTP_POST_BODY;
+    this.commands = {};
     this.hooks = {
-      "before:welcome:hello": this.beforeWelcome.bind(this),
-      "welcome:hello": this.welcomeUser.bind(this),
-      "welcome:world": this.displayHelloMessage.bind(this),
-      "after:welcome:world": this.afterHelloWorld.bind(this),
       "aws:deploy:finalize:cleanup": this.afterDeploy.bind(this),
     };
   }
 
-  beforeWelcome() {
-    this.serverless.cli.log("Hello from Serverless!");
-  }
-
-  welcomeUser() {
-    this.serverless.cli.log("Your message:");
-  }
-
-  displayHelloMessage() {
-    this.serverless.cli.log(`${this.options.message}`);
-  }
-
-  afterHelloWorld() {
-    this.serverless.cli.log("Please come again!");
-  }
-
-  beforeDeploy() {
-    console.log("in before deploy");
-    this.serverless.cli.log("Before deploy.");
-  }
-
-  afterDeploy() {
-    console.log(
-      "CLIENT_ID: ",
-      this.serverless.service.provider.environment.CTP_PROJECT_KEY
-    );
-
+  async afterDeploy() {
+    let service = null;
+    const projectKey = this.projectKey;
     this.serverless.cli.log(
-      "Creating commercetools subcription based on yaml specifications."
+      "Function deployed. Creating commercetools integration based on yaml specifications for project: " +
+        projectKey
     );
+    this.serverless.cli.log("Creating: " + this.deployType);
+    const authMiddleware = middlewareAuth.createAuthMiddlewareForClientCredentialsFlow(
+      {
+        host: this.authURL,
+        projectKey: projectKey,
+        credentials: {
+          clientId: this.clientId,
+          clientSecret: this.clientSecret,
+        },
+      }
+    );
+    const httpMiddleware = createMiddleware.createHttpMiddleware({
+      host: this.apiUrl,
+    });
+    const queueMiddleware = clientQueue.createQueueMiddleware({
+      concurrency: 5,
+    });
+    const client = clientCreate.createClient({
+      middlewares: [authMiddleware, httpMiddleware, queueMiddleware],
+    });
+    if (this.deployType === "extension") {
+      service = requestBuilder.createRequestBuilder({ projectKey }).extensions;
+    }
+    if (this.deployType === "subscription") {
+      service = requestBuilder.createRequestBuilder({ projectKey })
+        .subscriptions;
+    }
+    const url = service.build();
+    const postRequest = {
+      uri: url,
+      method: "POST",
+      body: this.postBody,
+    };
+    const result = await client.execute(postRequest);
+    this.serverless.cli.log("result: " + JSON.stringify(result));
   }
 }
 
